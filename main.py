20309c150e8ff96ba88e31cb1047209dcd313a44
arthmlvs <artemijs.hmelovs@gmail.com>
2024-12-03 09:11:33 +0400

    put image and text on screen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption("pygame artemijs")
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
@@ -9,10 +9,20 @@ pygame.display.set_icon(icon)
 square = pygame.Surface((50, 50))
 square.fill('Orange')
 
+myfont = pygame.font.Font('fonts/YujiMai-Regular.ttf', 40)
+text_surface = myfont.render('itartemijs', False, 'blue')
+
+player = pygame.image.load('images/icon.png')
+
 running = True 
 while running:
 
     screen.blit(square, (600/2-50/2, 300/2-50/2))
+    screen.blit(player, (0, 0))
+    screen.blit(text_surface, (500, 100))
+
+
+
 
     pygame.draw.circle(square, 'blue', (1, 1), 6)
     pygame.display.update()
